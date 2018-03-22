a9a5d8e6486fe187afc8c119a34ed32820ae88bb
Jamie Barton <hi@jamiebarton.co.uk>
2018-03-22 14:45:45 +0000

    Add part 09

---
+++ 09/components/CartSummary.js
@@ -0,0 +1,31 @@
+import StripeCheckout from 'react-stripe-checkout'
+import { Button, Segment, Divider } from 'semantic-ui-react'
+
+const stripeKey = '...'
+
+export default ({
+  handleCheckout,
+  display_price: { with_tax: { amount, currency, formatted } }
+}) => (
+  <React.Fragment>
+    <Divider />
+    <Segment clearing size="large">
+      <strong>Sub total:</strong> {formatted}
+      <StripeCheckout
+        name="NextJS Demo Store"
+        amount={amount}
+        currency={currency}
+        stripeKey={stripeKey}
+        shippingAddress={false}
+        billingAddress={true}
+        zipCode={true}
+        token={handleCheckout}
+        reconfigureOnUpdate={false}
+        triggerEvent="onClick">
+        <Button color="black" floated="right">
+          Check out
+        </Button>
+      </StripeCheckout>
+    </Segment>
+  </React.Fragment>
+)

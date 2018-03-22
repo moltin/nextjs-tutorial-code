747b9f7e5ae143472cf864d61aa63e54ee9b7344
Jamie Barton <hi@jamiebarton.co.uk>
2018-03-22 14:45:23 +0000

    Add part 04

---
+++ 04/lib/moltin.js
@@ -0,0 +1,12 @@
+import { gateway as MoltinGateway } from '@moltin/sdk'
+
+const Moltin = MoltinGateway({
+  client_id: '...'
+})
+
+export const getProducts = () => Moltin.Products.With('main_image').All()
+
+export const getProductById = id => Moltin.Products.With('main_image').Get(id)
+
+export const addToCart = (productId, quantity) =>
+  Moltin.Cart().AddProduct(productId, quantity)
